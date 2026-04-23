9f8c124d70eaa4d50ff5f79a8958eeadcb32641e
Ruan-Botha313 <bothar2@stu.otc.school.nz>
2026-04-24 09:39:32 +1200

    Changed questions list into a dictionary to hold answers, got rid of original answer holder

--- A Minecraft Movie Quiz.py
+++ A Minecraft Movie Quiz.py
@@ -14,10 +14,10 @@ order = [0,1,2,3,4,5,6,7,8,9]
 random.shuffle(order)
 
 #The questions
-questions = ["Who was the main character of the Minecraft Movie", "Which Line made the cinema's all over the world go wild", "Who was the main antagonist of the movie",
-             "Which object let the Protaginists of the film enter the Minecraft world", "Henry was known in the film for being...", "What was Steve's Dog name",
-             "Which one of these is not a line in the film", "What was the piglin Generals Name", "Which of these Youtubers were not mentioned or appeared in the film",
-             "What was the main sideplot of the movie"]
+questions = {"Who was the main character of the Minecraft Movie": 2, "Which Line made the cinema's all over the world go wild": 1, "Who was the main antagonist of the movie": 2,
+             "Which object let the Protaginists of the film enter the Minecraft world": 4, "Henry was known in the film for being...": 3, "What was Steve's Dog name": 2,
+             "Which one of these is not a line in the film": 3, "What was the piglin Generals Name": 1, "Which of these Youtubers were not mentioned or appeared in the film": 2,
+             "What was the main sideplot of the movie": 1}
 
 #The answers
 answers = ["1. John \n2. Steve \n3. Jeff \n4. Bowser", "1. CHICKEN JOCKEY! \n2. FLINT AND STEEL! \n3. I. AM STEVE! \n4. THE NETHER!", 
@@ -28,27 +28,6 @@ answers = ["1. John \n2. Steve \n3. Jeff \n4. Bowser", "1. CHICKEN JOCKEY! \n2. 
 
 #Repeats the question program
 for i in order:
-    if i == 0: 
-        answer = 2
-    if i == 1:
-        answer = 1
-    if i == 2:
-        answer = 2
-    if i == 3:
-        answer = 4
-    if i == 4:
-        answer = 3
-    if i == 5:
-        answer = 2
-    if i == 6:
-        answer = 3
-    if i == 7:
-        answer = 1
-    if i == 8:
-        answer = 2
-    if i == 9:
-        answer = 1
-    
     os.system('cls')
     print(questions[i])
     print(answers[i])
@@ -72,6 +51,8 @@ for i in order:
         input("Click enter to continue")
         
 print(f"Good job! you got {total_correct}0%")
+
+    
 
 #Random Hatsune Miku Cause Why Not
 """⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣠⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⢀⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
